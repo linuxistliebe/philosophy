e8695f935b5ca64654665639e00d3a891da270ae
linuxistliebe <linuxistliebe@gmail.com>
2025-11-13 17:42:48 +0530

    first commit

---
+++ effortless.py
@@ -0,0 +1,15 @@
+# -*- coding: utf-8 -*-
+"""
+Created on Thu Nov 13 17:33:23 2025
+
+@author: prashray
+"""
+
+universe=True
+while universe:
+    life=True
+    effort=True
+    while effort=True:
+        struggle=True
+    if effort==False:
+        effortless=True
